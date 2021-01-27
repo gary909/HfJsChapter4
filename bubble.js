f97aadfca79cce22abad57ac5d665ccd8ca53897
gary909 <garywhite61@hotmail.com>
2021-01-27 16:04:23 +0100

    Bubble code

---
+++ bubble.js
@@ -0,0 +1,11 @@
+var products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
+var hasBubbleGum = [false, false, false, true];
+
+for (var i = 0; i < hasBubbleGum.length; i = i + 1) {
+    if (hasBubbleGum[i] == true) {
+        console.log(products[i] + " contains bubblegum");
+    } else {
+        console.log("Nope");
+    }
+}
+
